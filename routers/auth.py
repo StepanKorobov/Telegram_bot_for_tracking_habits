4bf95b0a6971e8c884aff0e3e9a384ed3d1395e5
stepa <stepa.korobov@gmail.com>
2025-02-05 23:59:01 +0400

    added: 1) connect to database 2) test create and model 3) docker compose for database

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,10 @@
+import sys
+import os
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
+
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../database')))
+from database.models import sessions, create_tables, add_users
 
 router = APIRouter()
 
@@ -9,16 +14,18 @@ async def register():
     """
     Регистрация пользователей
     """
-    pass
+    return {"message": "Hello World"}
 
 
 @router.get("/auth/token")
 async def user_me() -> JSONResponse:
+    await create_tables()
     return JSONResponse({"message": "Hello World"})
 
 
 @router.get("/auth/login")
 async def user_login():
+    await add_users()
     return {"message": "Hello World"}
 
 
