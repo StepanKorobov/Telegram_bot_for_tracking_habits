fe33118ffd1da63bd6cd7d381f22b9f243d85c9b
stepa <stepa.korobov@gmail.com>
2025-02-08 19:48:24 +0400

    update database table create shema file

--- database/database.py
+++ database/database.py
@@ -5,13 +5,13 @@ from sqlalchemy.orm import (
     Relationship,
     declarative_base,
     mapped_column,
-    sessionmaker,
+    sessionmaker, relationship,
 )
 from sqlalchemy.orm.decl_api import DeclarativeMeta
 from sqlalchemy.engine.result import ChunkedIteratorResult
 from sqlalchemy.future import select
 import datetime
-from typing import List
+from typing import List, Optional
 
 DATABASE_URL: str = "postgresql+asyncpg://admin:admin@database:5432/telegram"
 engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
@@ -47,6 +47,9 @@ class Users(Base):
     password: Mapped[int] = mapped_column(String(50), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
+    # Определяем связь One-to-Many с таблицей Habits
+    habits: Mapped[Optional["Habits"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
 
 class Habits(Base):
     """Таблица привычек"""
@@ -58,21 +61,26 @@ class Habits(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     habit_name: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str] = mapped_column(String(250), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey(Users.id), nullable=False)
 
-    # Определяем связь
-    user_id: Mapped[List["Users"]] = Relationship(back_populates="habits", cascade="all, delete-orphan")
+    # Определяем связь Many-to-One с таблицей Users
+    user: Mapped[List["Users"]] = Relationship(back_populates="habits", cascade="all, delete-orphan")
+    # # Определяем связь One-to-Many с таблицей HabitTracking
+    habit_tracking: Mapped[Optional["HabitTracking"]] = relationship(back_populates="habits",
+                                                                     cascade="all, delete-orphan")
 
 
 class HabitTracking(Base):
     """Таблица для уведомлений"""
 
     # Название таблицы
-    __tablename__ = "habittracking"
+    __tablename__ = "habit_tracking"
 
     # Определяем поля таблицы
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     alert_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     count: Mapped[int] = mapped_column(Integer, nullable=False)
+    habits_id: Mapped[int] = mapped_column(ForeignKey(Habits.id), nullable=False)
 
-    # Определяем связь
-    habit_id: Mapped[List["Habits"]] = Relationship(backref="habit_tracking", cascade="all, delete-orphan")
+    # Определяем связь Many-to-One с таблицей Habits
+    habits: Mapped[List["Habits"]] = Relationship(back_populates="habit_tracking", cascade="all, delete-orphan")
