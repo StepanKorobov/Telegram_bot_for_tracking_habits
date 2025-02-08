02ccdcb4d4436c6677ffd671be719eb72652a76b
stepa <stepa.korobov@gmail.com>
2025-02-08 16:08:54 +0400

    update database model added poetry

--- database/database.py
+++ database/database.py
@@ -10,6 +10,8 @@ from sqlalchemy.orm import (
 from sqlalchemy.orm.decl_api import DeclarativeMeta
 from sqlalchemy.engine.result import ChunkedIteratorResult
 from sqlalchemy.future import select
+import datetime
+from typing import List
 
 DATABASE_URL = "postgresql+asyncpg://admin:admin@database:5432/telegram"
 engine = create_async_engine(DATABASE_URL, echo=True)
@@ -26,31 +28,36 @@ class Users(Base):
     """Таблица пользователей"""
 
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True)
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user: Mapped[str] = mapped_column(String(50), nullable=False)
     telegram_id: Mapped[int] = mapped_column(Integer, nullable=False)
     password: Mapped[int] = mapped_column(String(50), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
 
+class Habits(Base):
+    """Таблица привычек"""
+
+    __tablename__ = "habits"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    habit_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    user_id: Mapped[List["Users"]] = Relationship(back_populates="habits", cascade="all, delete-orphan")
+
+
+class HabitTracking(Base):
+    """Таблица для уведомлений"""
+
+    __tablename__ = "habittracking"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    alert_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    count: Mapped[int] = mapped_column(Integer, nullable=False)
+    habit_id: Mapped[List["Habits"]] = Relationship(backref="habit_tracking", cascade="all, delete-orphan")
+
+
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-
-async def add_users():
-    async with async_session() as session:
-        async with session.begin():
-            users_query = select(Users)
-            users_result: ChunkedIteratorResult = await session.execute(
-                users_query
-            )
-            users: Users = users_result.scalars().all()
-
-            if len(users) == 0:
-                user1 = Users(user="Test", telegram_id=113, password=3123123, is_active=True)
-                user2 = Users(user="Test1", telegram_id=113, password=3123123, is_active=True)
-                user3 = Users(user="Test2", telegram_id=113, password=3123123, is_active=True)
-                user4 = Users(user="Test3", telegram_id=113, password=3123123, is_active=True)
-                session.add_all([user1, user2, user3, user4])
-                session.commit()
