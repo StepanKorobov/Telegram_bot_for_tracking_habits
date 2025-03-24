c5cc7fdfe3a444d1f9a992acf0cceb3453885668
stepa <stepa.korobov@gmail.com>
2025-03-24 17:47:49 +0400

    added router for alert habits

--- routers/habit_tracking_router.py
+++ routers/habit_tracking_router.py
@@ -3,3 +3,33 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+@router.get("/habits_tracing/")
+async def habits_tracing():
+    # Получить все привычки со временем
+    pass
+
+
+@router.post("/habits_tracking/")
+async def create_habits_tracking():
+    # Создать трекинг для привычки
+    pass
+
+
+@router.patch("/habits_tracking/alert_time/")
+async def update_habits_tracking_alert_time():
+    # Обновить время оповещения
+    pass
+
+
+@router.patch("/habits_tracking/count/")
+async def update_habits_tracking_count():
+    # обновить количество
+    pass
+
+
+@router.delete("/habits_tracking/")
+async def delete_habits_tracking():
+    # Удалить трекинг
+    pass
+
+# count +1
