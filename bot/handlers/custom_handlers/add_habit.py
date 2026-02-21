e1f91bd9aa32db07e0044de4e65206baa427e827
Stepan <stepa.korobov@gmail.com>
2026-02-21 14:08:36 +0400

    Full implementation of the habit adding function

--- bot/handlers/custom_handlers/add_habit.py
+++ bot/handlers/custom_handlers/add_habit.py
@@ -1,60 +1,113 @@
-from typing import Dict
+from datetime import date, timedelta
 
-from telebot.types import Message
-
-from api.authentication import get_token, registration_user
 from api.habit_client import add_habit_api
-from database.models import add_user, get_user_by_telegram_id
+from bot.database.database import User
 from loader import bot
 from states.add_habit import HabitState
-from utils.password_validation import password_validator
-from utils.user_decorator import with_current_user
-from bot.database.database import User
+from telebot.types import CallbackQuery, Message
+from telegram_bot_calendar import WMonthTelegramCalendar
+from utils.user_decorator import (
+    check_user_registration,
+    get_current_user_from_inline_button,
+    with_current_user,
+)
 
 
 @bot.message_handler(commands=["add_habit"])
+@check_user_registration
 def add_hobbits(message: Message):
+    """Запускаем сценарий добавления новой привычки"""
+
     bot.set_state(message.from_user.id, HabitState.name, message.chat.id)
     bot.send_message(message.from_user.id, "Введите название новой привычки:")
 
 
 @bot.message_handler(state=HabitState.name)
 def process_habit_name(message: Message):
+    """Обработчик названия привычки"""
+
     bot.send_message(message.from_user.id, "Введите описание новой привычки:")
     bot.set_state(message.from_user.id, HabitState.description, message.chat.id)
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data["name"] = message.text
+        data["name"]: str = message.text
 
 
 @bot.message_handler(state=HabitState.description)
 def process_habit_description(message: Message):
+    """Обработчик описания привычки"""
+
     bot.send_message(message.from_user.id, "Введите цель новой привычки:")
     bot.set_state(message.from_user.id, HabitState.goal, message.chat.id)
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data["description"] = message.text
+        data["description"]: str = message.text
 
 
 @bot.message_handler(state=HabitState.goal)
 def process_habit_goal(message: Message):
-    bot.send_message(message.from_user.id, "Введите сроки выполнения новой привычки:")
+    """Обработчик цели привычки"""
+
+    current_min_date: date = date.today() + timedelta(days=21)
+    calendar, step = WMonthTelegramCalendar(
+        locale="ru",
+        current_date=current_min_date,
+        min_date=current_min_date,
+        max_date=date.today() + timedelta(days=21 * 3),
+    ).build()
+
+    bot.send_message(
+        message.chat.id,
+        f"Выберете дату выполнения новой привычки:",
+        reply_markup=calendar,
+    )
     bot.set_state(message.from_user.id, HabitState.terms, message.chat.id)
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data["goal"] = message.text
+        data["goal"]: str = message.text
+
+
+@bot.callback_query_handler(state=HabitState.terms, func=WMonthTelegramCalendar.func())
+@get_current_user_from_inline_button
+def cal(call: CallbackQuery, current_user: User):
+    """Обработчик inline календаря, так же после выбора даты делает запрос к API"""
+
+    current_min_date: date = date.today() + timedelta(days=21)
+    result, key, step = WMonthTelegramCalendar(
+        locale="ru",
+        current_date=current_min_date,
+        min_date=current_min_date,
+        max_date=date.today() + timedelta(days=21 * 3),
+    ).process(call.data)
+
+    if not result and key:
+        bot.edit_message_text(
+            f"Выберете дату:",
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=key,
+        )
+    elif result:
+        bot.edit_message_text(
+            f"Вы выбрали {result}", call.message.chat.id, call.message.message_id
+        )
+
+        with bot.retrieve_data(call.from_user.id) as data:
+            data["terms"]: date = result
+
+        result: bool = add_habit_api(user=current_user, habit_data=data)
+        if result:
+            bot.send_message(call.from_user.id, "Новая привычка успешно добавлена!")
+        else:
+            bot.send_message(call.from_user.id, "Не удалось добавить привычку.")
+
+        bot.delete_state(call.from_user.id, call.message.chat.id)
 
 
 @bot.message_handler(state=HabitState.terms)
-@with_current_user
-def process_habit_terms(message: Message, current_user: User):
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data["terms"] = message.text
+def process_habit_terms(message: Message):
+    """Когда активен календарь, выводит сообщение пользователю"""
 
-    result = add_habit_api(user=current_user, habit_data=data)
-    if result:
-        bot.send_message(message.from_user.id, "Новая привычка успешно добавлена!")
-    else:
-        bot.send_message(message.from_user.id, "Не удалось добавить привычку.")
-
-    bot.delete_state(message.from_user.id, message.chat.id)
+    bot.send_message(
+        message.from_user.id, "Ошибка: необходимо выбрать дату в меню выше."
+    )
--- bot/utils/user_decorator.py
+++ bot/utils/user_decorator.py
@@ -1,6 +1,8 @@
 from functools import wraps
 from bot.database.database import Base, User, get_session
-from bot.database.models import get_user_by_telegram_id
+from bot.database.models import get_user_by_telegram_id, check_user_by_telegram_id
+from loader import bot
+from states.login import LoginState
 
 
 def with_current_user(func):
@@ -9,5 +11,35 @@ def with_current_user(func):
         current_user = get_user_by_telegram_id(telegram_id=message.from_user.id)
         # Можно ещё положить db
         return func(message, current_user=current_user, *args, **kwargs)
+
+    return wrapper
+
+
+def get_current_user_from_inline_button(func):
+    @wraps(func)
+    def wrapper(call, *args, **kwargs):
+        current_user = get_user_by_telegram_id(telegram_id=call.from_user.id)
+        return func(call, current_user=current_user, *args, **kwargs)
+
+    return wrapper
+
+
+def check_user_registration(func):
+    """
+    Декоратор проверяет зарегистрирован ли пользователь в боте
+    """
+
+    @wraps(func)
+    def wrapper(message, *args, **kwargs):
+        user_exist = check_user_by_telegram_id(telegram_id=message.from_user.id)
+        if user_exist:
+            # return func(message, current_user=current_user, *args, **kwargs)
+            return func(message, *args, **kwargs)
+
+        bot.send_message(
+            message.from_user.id,
+            f"Привет {message.from_user.full_name}, ты ещё не зарегистрирован! Для регистрации введи пароль:",
+        )
+        bot.set_state(message.from_user.id, LoginState.registration, message.chat.id)
 
     return wrapper
