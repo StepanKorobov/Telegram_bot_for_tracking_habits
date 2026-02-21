e5ab78138d90316eedc14aed625d25c03ea113aa
Stepan <stepa.korobov@gmail.com>
2026-02-21 17:57:30 +0400

    Full implementation of the list of all habits

--- bot/handlers/custom_handlers/__init__.py
+++ bot/handlers/custom_handlers/__init__.py
@@ -2,5 +2,6 @@ from . import login
 from . import add_habit
 from . import get_habit
 from . import edit_habit
+from . import track_habit
 
-__all__ = ['login', 'add_habit', 'get_habit', 'edit_habit']
+__all__ = ["login", "add_habit", "get_habit", "edit_habit", "track_habit"]
