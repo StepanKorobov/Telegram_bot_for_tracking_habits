c2dc633185932a0db6b78408668549a247e4d0c3
Stepan <stepa.korobov@gmail.com>
2026-04-02 00:43:16 +0400

    add inline button for edit habit

--- bot/handlers/custom_handlers/edit_habit.py
+++ bot/handlers/custom_handlers/edit_habit.py
@@ -1,15 +1,15 @@
 from typing import Dict
 
-from telebot.types import Message
-
+from telebot.types import Message, CallbackQuery
 from api.authentication import get_token, registration_user
 from api.habit_client import add_habit_api, get_habit_api
 from database.models import add_user, get_user_by_telegram_id
 from loader import bot
 from states.edit_habit import EditState
 from utils.password_validation import password_validator
-from utils.user_decorator import with_current_user
+from utils.user_decorator import with_current_user, get_current_user_from_inline_button
 from bot.database.database import User
+from bot.keyboards.inline.edit_habit import edit_habits_keyboard, edit_hobit_id_keyboard, edit_hobit_id_choice_keyboard
 
 
 @bot.message_handler(commands=["edit_habit"])
@@ -18,9 +18,9 @@ def edit_habit(message: Message, current_user: User):
     habits = get_habit_api(user=current_user)
     if habits:
         bot.set_state(message.from_user.id, EditState.edit, message.chat.id)
-        habits_list = [f"ID: {i["id"]}, name: {i["habit_name"]}" for i in habits]
-        habits = "\n".join(habits_list)
-        bot.send_message(message.chat.id, f"Введите ID привычки для редактирования:\n{habits}")
+        habits_list = [{"id": i["id"], "name": i["habit_name"]} for i in habits]
+        bot.send_message(message.chat.id, f"Выберете привычку для редактирования:",
+                         reply_markup=edit_habits_keyboard(habits_list=habits_list))
     else:
         bot.send_message(message.chat.id, "У Вас пока ещё нет привычек")
 
@@ -81,8 +81,87 @@ def process_edit_terms(message: Message):
     bot.delete_state(message.from_user.id, message.chat.id)
 
 
-@bot.message_handler(state=EditState.remove)
-def process_edit_remove(message: Message):
-    # Процесс удаления привычки
-    bot.send_message("Привычка возможно удалена.")
-    bot.delete_state(message.from_user.id, message.chat.id)
+# @bot.message_handler(state=EditState.remove)
+# def process_edit_remove(message: Message):
+#     # Процесс удаления привычки
+#     bot.send_message("Привычка возможно удалена.")
+#     bot.delete_state(message.from_user.id, message.chat.id)
+
+@bot.callback_query_handler(state=EditState.edit, func=lambda call: call.data.startswith("habit_id_"))
+@get_current_user_from_inline_button
+def edit_hobit(call: CallbackQuery, current_user: User):
+    """Выбор действия редактировать или удалить"""
+    habit_id = int(call.data.split("_")[2])
+    habit_name = call.data.split("_")[3]
+    bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=f"{habit_name}",
+        reply_markup=edit_hobit_id_keyboard(habit_id=habit_id)
+    )
+
+
+@bot.callback_query_handler(state=EditState.edit, func=lambda call: call.data.startswith("habit_edit_id_"))
+@get_current_user_from_inline_button
+def edit_hobit_id_choice_action(call: CallbackQuery, current_user: User):
+    """Выбор того, что именно редактировать в привычке или всё"""
+    habit_id = int(call.data.split("_")[3])
+    bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text="Выберете, что редактировать:",
+        reply_markup=edit_hobit_id_choice_keyboard(habit_id=habit_id)
+    )
+
+
+@bot.callback_query_handler(state=EditState.edit, func=lambda call: call.data.startswith("habit_choice_id_"))
+@get_current_user_from_inline_button
+def edit_hobit_id_choice_action_all(call: CallbackQuery, current_user: User):
+    """Обработка выбранного действия"""
+    habit_action = call.data.split("_")[3]
+    habit_id = int(call.data.split("_")[4])
+    bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=f"{habit_id}, {habit_action}",
+        reply_markup=None
+    )
+
+
+@bot.callback_query_handler(state=EditState.edit, func=lambda call: call.data.startswith("habit_remove_id_"))
+@get_current_user_from_inline_button
+def delete_habits_id(call: CallbackQuery, current_user: User):
+    """Удаление привычки пользователя по id"""
+    habit_id = int(call.data.split("_")[3])
+    bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text="Привычка успешно удалена!",
+        reply_markup=None
+    )
+
+
+@bot.callback_query_handler(state=EditState.edit, func=lambda call: call.data == "delete_all_habit")
+@get_current_user_from_inline_button
+def delete_all_habits(call: CallbackQuery, current_user: User):
+    """Удаление всех привычек пользователя"""
+    bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text="Все привычки успешно удалены!",
+        reply_markup=None
+    )
+    bot.delete_state(call.from_user.id, call.message.chat.id)
+
+
+@bot.callback_query_handler(state=EditState.edit, func=lambda call: call.data == "clear_menu")
+def clear_menu(call: CallbackQuery):
+    """Убрать меню"""
+    bot.edit_message_reply_markup(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        reply_markup=None
+    )
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+    bot.answer_callback_query(call.id, "Клавиатура убрана!")
+    bot.delete_state(call.from_user.id, call.message.chat.id)
---
+++ bot/keyboards/inline/edit_habit.py
@@ -0,0 +1,38 @@
+from typing import List, Dict
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def edit_habits_keyboard(habits_list: List) -> InlineKeyboardMarkup:
+    """
+    Клавиатура редактирования привычек
+
+    :param habits_list: Список привычек
+    :type habits_list: List
+    :return: Клавиатуру
+    :rtype: InlineKeyboardMarkup
+    """
+
+    kb = InlineKeyboardMarkup()
+    for i_id, i_habit in enumerate(habits_list):
+        kb.row(InlineKeyboardButton(text=f"{i_id + 1}) {i_habit["name"]}",
+                                    callback_data=f"habit_id_{i_habit["id"]}_{i_habit["name"]}"))
+    kb.row(InlineKeyboardButton(text="Удалить все привычки", callback_data="delete_all_habit"),
+           InlineKeyboardButton(text="Закрыть", callback_data="clear_menu"), )
+    return kb
+
+
+def edit_hobit_id_keyboard(habit_id: int) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup()
+    kb.row(InlineKeyboardButton(text="Редактировать", callback_data=f"habit_edit_id_{habit_id}"),
+           InlineKeyboardButton(text="Удалить", callback_data=f"habit_remove_id_{habit_id}"))
+    return kb
+
+
+def edit_hobit_id_choice_keyboard(habit_id: int) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup()
+    kb.row(InlineKeyboardButton(text="Название", callback_data=f"habit_choice_id_name_{habit_id}"))
+    kb.row(InlineKeyboardButton(text="Описание", callback_data=f"habit_choice_id_description_{habit_id}"))
+    kb.row(InlineKeyboardButton(text="Цель", callback_data=f"habit_choice_id_goal_{habit_id}"))
+    kb.row(InlineKeyboardButton(text="Срок", callback_data=f"habit_choice_id_terms_date_{habit_id}"))
+    kb.row(InlineKeyboardButton(text="Всё", callback_data=f"habit_choice_id_all_{habit_id}"))
+    return kb
