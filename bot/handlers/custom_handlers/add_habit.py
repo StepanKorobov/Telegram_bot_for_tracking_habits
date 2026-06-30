59d85ab4f4c836968f8870560641ad60dab2bc96
Stepan <stepa.korobov@gmail.com>
2026-06-30 20:03:04 +0400

    Рефакторинг кода Добавлен docstring Добавлены аннотации типов

--- bot/handlers/custom_handlers/add_habit.py
+++ bot/handlers/custom_handlers/add_habit.py
@@ -15,117 +15,221 @@ from utils.user_decorator import (
 
 @bot.message_handler(commands=["add_habit"])
 @check_user_registration
-def add_hobbits(message: Message):
-    """Запускаем сценарий добавления новой привычки"""
+def add_hobbit(message: Message) -> None:
+    """
+    Запускаем сценарий добавления новой привычки
 
-    bot.set_state(message.from_user.id, HabitState.name, message.chat.id)
-    bot.send_message(message.from_user.id, "Введите название новой привычки:")
+    Args:
+        message: Сообщение с данными
+
+    Returns:
+        None
+    """
+
+    user_id: int = message.from_user.id
+    chat_id: int = message.chat.id
+
+    bot.set_state(
+        user_id=user_id,
+        state=HabitState.name,
+        chat_id=chat_id,
+    )
+    bot.send_message(chat_id=chat_id, text="Введите название новой привычки:")
 
 
 @bot.message_handler(state=HabitState.name)
-def process_habit_name(message: Message):
-    """Обработчик названия привычки"""
+def process_habit_name(message: Message) -> None:
+    """
+    Обработчик названия привычки
 
-    habit_name = message.text
+    Args:
+        message: Сообщение с данными
+
+    Returns:
+        None
+    """
+
+    user_id: int = message.from_user.id
+    chat_id: int = message.chat.id
+    habit_name: str = message.text
+
     if len(habit_name) <= 50:
-        bot.send_message(message.from_user.id, "Введите описание новой привычки:")
-        bot.set_state(message.from_user.id, HabitState.description, message.chat.id)
+        bot.send_message(
+            chat_id=chat_id,
+            text="Введите описание новой привычки:",
+        )
+        bot.set_state(
+            user_id=user_id,
+            state=HabitState.description,
+            chat_id=chat_id,
+        )
 
-        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-            data["name"]: str = habit_name
+        with bot.retrieve_data(user_id=user_id, chat_id=chat_id) as data:
+            data["name"] = habit_name
     else:
         bot.send_message(
-            message.from_user.id, "Название может содержать не более 50 символов."
+            chat_id=chat_id,
+            text="Название может содержать не более 50 символов.",
         )
 
 
 @bot.message_handler(state=HabitState.description)
-def process_habit_description(message: Message):
-    """Обработчик описания привычки"""
+def process_habit_description(message: Message) -> None:
+    """
+    Обработчик описания привычки
 
+    Args:
+        message: Сообщение с данными
+
+    Returns:
+        None
+    """
+
+    user_id: int = message.from_user.id
+    chat_id: int = message.chat.id
     hobbit_description = message.text
-    if len(hobbit_description) <= 250:
-        bot.send_message(message.from_user.id, "Введите цель новой привычки:")
-        bot.set_state(message.from_user.id, HabitState.goal, message.chat.id)
 
-        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-            data["description"]: str = message.text
+    if len(hobbit_description) <= 250:
+        bot.send_message(
+            chat_id=chat_id,
+            text="Введите цель новой привычки:",
+        )
+        bot.set_state(
+            user_id=user_id,
+            state=HabitState.goal,
+            chat_id=chat_id,
+        )
+
+        with bot.retrieve_data(user_id=user_id, chat_id=chat_id) as data:
+            data["description"] = message.text
     else:
         bot.send_message(
-            message.from_user.id, "Описание может содержать не более 250 символов."
+            chat_id=chat_id,
+            text="Описание может содержать не более 250 символов.",
         )
 
 
 @bot.message_handler(state=HabitState.goal)
-def process_habit_goal(message: Message):
-    """Обработчик цели привычки"""
+def process_habit_goal(message: Message) -> None:
+    """
+    Обработчик цели привычки
 
-    habit_goal = message.text
+    Args:
+        message: Сообщение с данными
+
+    Returns:
+        None
+    """
+
+    user_id: int = message.from_user.id
+    chat_id: int = message.chat.id
+    habit_goal: str = message.text
+
     if len(habit_goal) <= 50:
         current_min_date: date = date.today() + timedelta(days=21)
+        max_date: date = date.today() + timedelta(days=21 * 3)
         calendar, step = WMonthTelegramCalendar(
             locale="ru",
             current_date=current_min_date,
             min_date=current_min_date,
-            max_date=date.today() + timedelta(days=21 * 3),
+            max_date=max_date,
         ).build()
 
         bot.send_message(
-            message.chat.id,
-            f"Выберете дату выполнения новой привычки:",
+            chat_id=chat_id,
+            text=f"Выберете дату выполнения новой привычки:",
             reply_markup=calendar,
         )
-        bot.set_state(message.from_user.id, HabitState.terms, message.chat.id)
+        bot.set_state(
+            user_id=user_id,
+            state=HabitState.terms,
+            chat_id=chat_id,
+        )
 
-        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-            data["goal"]: str = message.text
+        with bot.retrieve_data(user_id=user_id, chat_id=chat_id) as data:
+            data["goal"] = message.text
     else:
         bot.send_message(
-            message.from_user.id, "Цель может содержать не более 50 символов."
+            chat_id=chat_id,
+            text="Цель может содержать не более 50 символов.",
         )
 
 
 @bot.callback_query_handler(state=HabitState.terms, func=WMonthTelegramCalendar.func())
 @get_current_user_from_inline_button
-def cal(call: CallbackQuery, current_user: User):
-    """Обработчик inline календаря, так же после выбора даты делает запрос к API"""
+def cal(call: CallbackQuery, current_user: User) -> None:
+    """
+    Обработчик inline календаря, так же после выбора даты делает запрос к API
+
+    Args:
+        call: Данные с кнопки inline
+        current_user: текущий пользователь полученные из БД
+
+    Returns:
+        None
+    """
+
+    user_id: int = call.from_user.id
+    chat_id: int = call.message.chat.id
+    message_id: int = call.message.message_id
 
     current_min_date: date = date.today() + timedelta(days=21)
+    max_date: date = date.today() + timedelta(days=21 * 3)
     result, key, step = WMonthTelegramCalendar(
         locale="ru",
         current_date=current_min_date,
         min_date=current_min_date,
-        max_date=date.today() + timedelta(days=21 * 3),
+        max_date=max_date,
     ).process(call.data)
 
     if not result and key:
         bot.edit_message_text(
-            f"Выберете дату:",
-            call.message.chat.id,
-            call.message.message_id,
+            chat_id=chat_id,
+            message_id=message_id,
+            text=f"Выберете дату:",
             reply_markup=key,
         )
     elif result:
         bot.edit_message_text(
-            f"Вы выбрали {result}", call.message.chat.id, call.message.message_id
+            chat_id=chat_id,
+            message_id=message_id,
+            text=f"Вы выбрали {result}",
         )
 
         with bot.retrieve_data(call.from_user.id) as data:
-            data["terms"]: date = result
+            data["terms"] = result
 
         result: bool = add_habit_api(user=current_user, habit_data=data)
         if result:
-            bot.send_message(call.from_user.id, "Новая привычка успешно добавлена!")
+            bot.send_message(
+                chat_id=chat_id,
+                text="Новая привычка успешно добавлена!",
+            )
         else:
-            bot.send_message(call.from_user.id, "Не удалось добавить привычку.")
+            bot.send_message(
+                chat_id=chat_id,
+                text="Не удалось добавить привычку.",
+            )
 
-        bot.delete_state(call.from_user.id, call.message.chat.id)
+        bot.delete_state(
+            user_id=user_id,
+            chat_id=chat_id,
+        )
 
 
 @bot.message_handler(state=HabitState.terms)
 def process_habit_terms(message: Message):
-    """Когда активен календарь, выводит сообщение пользователю"""
+    """
+    Когда активен календарь, выводит сообщение пользователю
+
+    Args:
+        message: Сообщение с данными
+
+    Returns:
+        None
+    """
 
     bot.send_message(
-        message.from_user.id, "Ошибка: Необходимо выбрать дату в меню выше."
+        chat_id=message.chat.id,
+        text="Ошибка: Необходимо выбрать дату в меню выше.",
     )
--- bot/states/add_habit.py
+++ bot/states/add_habit.py
@@ -2,6 +2,8 @@ from telebot.handler_backends import State, StatesGroup
 
 
 class HabitState(StatesGroup):
+    """Состояния для добавления привычки."""
+
     name = State()
     description = State()
     goal = State()
