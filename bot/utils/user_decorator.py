f4c6202928c4a9173ec5ab4b144df08a3873650d
Stepan <stepa.korobov@gmail.com>
2026-07-02 02:56:01 +0400

    Рефакторинг: Добавлен docstring Добавлены аннотации типов Код проверен линтерами

--- bot/utils/user_decorator.py
+++ bot/utils/user_decorator.py
@@ -1,11 +1,11 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Any, Callable
 
+from api.authentication import TokenRefreshFailed
 from bot.database.database import User
 from bot.database.models import check_user_by_telegram_id, get_user_by_telegram_id
 from loader import bot
 from states.login import LoginState
-from api.authentication import TokenRefreshFailed
 
 
 def with_current_user(func: Callable) -> Callable:
@@ -13,31 +13,38 @@ def with_current_user(func: Callable) -> Callable:
 
     @wraps(func)
     def wrapper(message, *args, **kwargs) -> Any:
-        current_user: User | None = get_user_by_telegram_id(
-            telegram_id=message.from_user.id
-        )
+        user_id: int = message.from_user.id
+        chat_id: int = message.chat.id
+        full_name: str = message.from_user.full_name
+
+        current_user: User | None = get_user_by_telegram_id(telegram_id=user_id)
+
         # Можно ещё положить db
         if current_user:
             try:
                 result = func(message, current_user=current_user, *args, **kwargs)
             except TokenRefreshFailed as exc:
                 bot.send_message(
-                    chat_id=message.from_user.id,
-                    text=f"Привет {message.from_user.full_name}, ты давно не заходил! Для регистрации введи пароль:",
+                    chat_id=chat_id,
+                    text=f"Привет {full_name}, ты давно не заходил! Для входа введи пароль:",
                 )
                 bot.set_state(
-                    user_id=message.from_user.id,
+                    user_id=user_id,
                     state=LoginState.login,
-                    chat_id=message.chat.id,
+                    chat_id=chat_id,
                 )
             else:
                 return result
         else:
             bot.send_message(
-                message.from_user.id,
-                f"Привет {message.from_user.full_name}, ты ещё не зарегистрирован! Для регистрации введи пароль:",
+                chat_id=chat_id,
+                text=f"Привет {full_name}, ты ещё не зарегистрирован! Для регистрации введи пароль:",
             )
-            bot.set_state(message.from_user.id, LoginState.registration, message.chat.id)
+            bot.set_state(
+                user_id=user_id,
+                state=LoginState.registration,
+                chat_id=chat_id,
+            )
 
     return wrapper
 
@@ -60,15 +67,23 @@ def check_user_registration(func) -> Callable:
 
     @wraps(func)
     def wrapper(message, *args, **kwargs) -> Any:
-        user_exist: bool = check_user_by_telegram_id(telegram_id=message.from_user.id)
+        user_id: int = message.from_user.id
+        chat_id: int = message.chat.id
+        full_name: str = message.from_user.full_name
+
+        user_exist: bool = check_user_by_telegram_id(telegram_id=user_id)
         if user_exist:
             # return func(message, current_user=current_user, *args, **kwargs)
             return func(message, *args, **kwargs)
 
         bot.send_message(
             message.from_user.id,
-            f"Привет {message.from_user.full_name}, ты ещё не зарегистрирован! Для регистрации введи пароль:",
+            f"Привет {full_name}, ты ещё не зарегистрирован! Для регистрации введи пароль:",
         )
-        bot.set_state(message.from_user.id, LoginState.registration, message.chat.id)
+        bot.set_state(
+            user_id=user_id,
+            state=LoginState.registration,
+            chat_id=chat_id,
+        )
 
     return wrapper
