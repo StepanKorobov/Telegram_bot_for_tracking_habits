9e002848794edeca33919b563016c57b39dda607
Stepan <stepa.korobov@gmail.com>
2026-07-02 01:44:54 +0400

    Рефакторинг: Добавлен docstring Добавлены аннотации типов Код проверен линтерами

--- bot/states/login.py
+++ bot/states/login.py
@@ -2,5 +2,7 @@ from telebot.handler_backends import State, StatesGroup
 
 
 class LoginState(StatesGroup):
+    """Состояния для регистрации и входа в бота."""
+
     registration = State()
     login = State()
