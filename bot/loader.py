3f112ed5e6c7aaf1a939ce9b79623fbf0ee4721b
Stepan <stepa.korobov@gmail.com>
2026-07-19 21:09:26 +0400

    Добавлено логирование в загрузчик бота

--- bot/loader.py
+++ bot/loader.py
@@ -1,10 +1,15 @@
+import logging
+
 from telebot import TeleBot, apihelper
 from telebot.storage import StateMemoryStorage
 
 from bot.database.database import Base, engine
 from config_data import config
 
+logger = logging.getLogger(__name__)
+
 if config.PROXY:
+    logger.info("proxy loaded access")
     apihelper.proxy = {
         "https": f"socks5h://{config.PROXY}",
         "http": f"socks5h://{config.PROXY}",
